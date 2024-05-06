6fb09fc1dc8ef28f760e6c85e9b733cdab001c46
unknown <ron.garcia.santos@gmail.com>
2024-05-06 14:18:58 +0800

    feedback

--- App.tsx
+++ App.tsx
@@ -1,114 +1,43 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
-import type { PropsWithChildren } from 'react';
+import React, {Component} from 'react';
+import {Alert, Button, StyleSheet, View} from 'react-native';
 
-const AlignItemsLayout = () => {
-  const [alignItems, setAlignItems] = useState('stretch');
-  return(
-    <PreviewLayout
-      label='alignItems'
-      selectedValue={alignItems}
-      values={['stretch', 'flex-start', 'flex-end', 'center', 'baseline']}
-      setSelectedValue={setAlignItems}
-    >
-      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
-      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
-      <View 
-        style={[
-          styles.box,
-          {
-            backgroundColor: 'steelblue',
-            width: 'auto',
-            minWidth: 50,
-          }
-        ]}
-      />
-    </PreviewLayout>
-  )
-
+export default class ButtonBasics extends Component {
+  __onPressButton() {
+    Alert.alert('You tapped the button');
+  }
+  render() {
+    return(
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.__onPressButton} title='Press Me' />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this.__onPressButton}
+            title="Press Me"
+            color='#841584'
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this.__onPressButton} title="this looks great" />
+          <Button onPress={this.__onPressButton} title="Ok" />
+        </View>
+      </View>
+    )
+  }
 }
-
-type PreviewLayoutProps = PropsWithChildren<{
-  label: string;
-  values: string[];
-  selectedValue: string;
-  setSelectedValue: (value: string) => void;
-}>;
-
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}: PreviewLayoutProps) => (
-  <View style={{padding: 10, flex: 1}}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={(() => setSelectedValue(value))}
-          style={[styles.button, selectedValue === value && styles.selected]}
-        >
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel
-            ]}
-          >
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
-  </View>
-)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 8,
-    backgroundColor: 'aliceblue',
-    minHeight: 200,
+    justifyContent: 'center',
   },
-  box: {
-    width: 50,
-    height: 50,
+  buttonContainer: {
+    margin: 20
   },
-  row: {
+  alternativeLayoutButtonContainer: {
+    margin: 20,
     flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'oldlace',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  selected: {
-    backgroundColor: 'coral',
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'coral',
-  },
-  selectedLabel: {
-    color: 'white',
-  },
-  label: {
-    textAlign: 'center',
-    marginBottom: 10,
-    fontSize: 24,
+    justifyContent: 'space-between',
   },
 })
-
-export default AlignItemsLayout;
